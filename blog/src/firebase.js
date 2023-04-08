b418beb213a65dfdc2f8bf31a8135041b9cf0250
MeghavatiChaudhari <meghapjc29@gmail.com>
2023-04-08 17:33:16 +0530

    create-post form create and states add

---
+++ blog/src/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+const firebaseConfig = {
+  apiKey: "AIzaSyAyUB-BToNLuYI464K4dlgSQBdfx1PCXkM",
+  authDomain: "blog-30427.firebaseapp.com",
+  projectId: "blog-30427",
+  storageBucket: "blog-30427.appspot.com",
+  messagingSenderId: "422564972209",
+  appId: "1:422564972209:web:ead774f7d8158f10e8c490"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const firestore = firebase.firestore();
